4a7815d503c7ff786614ff7a960cd24155d95484
kinus-bit <mungaiallan540@gmail.com>
2025-10-30 16:10:30 +0300

    fakeusers

---
+++ frontend/src/data/fakeUsers.js
@@ -0,0 +1,5 @@
+export const fakeUsers = [
+    { id: 1, role: "admin", email: "admin@gmail.com", password: "admin123", name: "Admin" },
+    { id: 2, role: "patient", email: "patient@gmail.com", password: "patient123", name: "Allan" },
+  ];
+  
