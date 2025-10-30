87f4147587e4c388d20a0e700a049fa6d692066c
kinus-bit <mungaiallan540@gmail.com>
2025-10-30 16:10:22 +0300

    fake patients

---
+++ frontend/src/data/fakePatients.js
@@ -0,0 +1,5 @@
+export const fakePatients = [
+    { id: 1, name: "Allan kinuthia", email: "allan@gmail.com", checks: 5 },
+    { id: 2, name: "Joel Mungai", email: "joel@gmail.com.com", checks: 3 },
+  ];
+  
