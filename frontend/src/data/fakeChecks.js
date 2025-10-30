a3ff9bd357bca8fc384b337479707857919f33fe
kinus-bit <mungaiallan540@gmail.com>
2025-10-30 16:10:11 +0300

    fakechecks

---
+++ frontend/src/data/fakeChecks.js
@@ -0,0 +1,5 @@
+export const fakeChecks = [
+    { id: 1, userId: 2, symptoms: "Headache, fever", prediction: "Possible Flu", advice: "Rest and hydration", date: "2025-10-28" },
+    { id: 2, userId: 2, symptoms: "Chest pain, short breath", prediction: "Possible Heart Issue", advice: "See a doctor immediately", date: "2025-10-29" },
+  ];
+  
